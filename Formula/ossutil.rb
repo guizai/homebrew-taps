b89718c4acc2567598f4bc54f080bc0fae3719be
Guizai <guizaicn@gmail.com>
2021-03-12 12:28:14 +0800

    chore: bump ossutil version 1.6.13 -> 1.7.1

--- Formula/ossutil.rb
+++ Formula/ossutil.rb
@@ -3,7 +3,7 @@ class Ossutil < Formula
     homepage "https://github.com/aliyun/ossutil"
     version "1.7.1"
     url "https://gosspublic.alicdn.com/ossutil/#{version}/ossutilmac64"
-    sha256 "c3fbf9392a9af823d52b874e53c521fd99fabfccdd290b8ef677f7acac4cff57"
+    sha256 "cdab34aa7e46fe4f4398e7abac1efb24ce44876af4bb9d240cf1caebff05062c"
 
     def install
         bin.install "ossutilmac64" => "ossutil"
